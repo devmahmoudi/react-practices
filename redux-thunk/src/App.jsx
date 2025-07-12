966fd38c70e643504d85a4d0949d29c25083883e
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-12 17:43:23 +0330

    feat: loading users list using redux-thunk.

--- redux-thunk/src/App.jsx
+++ redux-thunk/src/App.jsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { usersListSelector, usersLoaded } from './features/user/userSlice'
+import { fetchUsers as fetchUserList } from './features/user/userSlice'
 
 function App() {
   const [count, setCount] = useState(0)
   const users = useSelector(usersListSelector)
   const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
 
   const fetchUsers = () => {
-    console.log('here');
-    
-    dispatch(usersLoaded([
-    {
-      name: "Mahdi",
-      family: "Mahmoudi",
-      phone: "09339279628"
-    }
-  ]))
+    // Dispatch Redux Thunk 
+    dispatch(fetchUserList())  
+
+    setLoading(true)
   }
+
+  useEffect(() => {
+    if(users.length)
+      setLoading(false)
+  }, [users])
 
   return (
     <>
       <h2>
         Redux Thunk Practice
       </h2>
+      {
+        loading && (
+          <h6>Loading ...</h6>
+        )
+      }
       <div>
         {
           (users.map((user, index) =>
--- redux-thunk/src/features/user/userSlice.js
+++ redux-thunk/src/features/user/userSlice.js
@@ -5,13 +5,13 @@ const userSlice = createSlice({
     name: 'user',
     initialState: { users: [], selectedUser: {} },
     reducers: {
-        usersLoaded: (state, action) => {state.users = action.payload},
-        selectedUserLoaded: (state, action) => {state.selectedUser = action.payload}
+        usersLoaded: (state, action) => { state.users = action.payload },
+        selectedUserLoaded: (state, action) => { state.selectedUser = action.payload }
     }
 })
 
 // ACTIONS
-export const {usersLoaded, selectedUserLoaded} = userSlice.actions 
+export const { usersLoaded, selectedUserLoaded } = userSlice.actions
 
 // REDUCER
 export const userReducer = userSlice.reducer
@@ -19,3 +19,27 @@ export const userReducer = userSlice.reducer
 // SELECTORS 
 export const usersListSelector = state => state.user.users
 export const selectedUserSelector = state => state.user.selectedUser
+
+// Redux Thunk
+export const fetchUsers = () => {
+    return async (dispatch, getState) => {
+        const users = await (new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve([
+                    {
+                        name: "Mahdi",
+                        family: "Mahmoudi",
+                        phone: "09123456789"
+                    },
+                    {
+                        name: "AmirAli",
+                        family: "Mahmoudi",
+                        phone: "091223234581"
+                    },
+                ])
+            }, 3000);
+        }))
+
+        dispatch(usersLoaded(users))
+    }
+}
