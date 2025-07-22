5a858a287596a9614c8d1a5c5283efc7e3db61e3
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-22 18:40:13 +0330

    feat: add the uerSlice.

--- redux-blog/src/store/index.js
+++ redux-blog/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import blogReducers from "../features/blogSlice"
+import userReducers from "../features/userSlice"
 
 const store = configureStore({
     reducer: {
-        blogs: blogReducers
+        blogs: blogReducers,
+        users: userReducers
     }
 })
 
