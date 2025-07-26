fa321f677b7c45acda6e8a9fc5adf6233c5f40df
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-26 18:38:52 +0330

    feat(userSlice): add extraReducers for fetchUsers and init state

--- redux-blog/src/features/userSlice.js
+++ redux-blog/src/features/userSlice.js
@@ -1,28 +1,20 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getAllUsers } from "../services/usersService";
 
-const initialState = {
-  users: [
-    {
-      id: 1,
-      fullname: "Mahdi Mahmoudi",
-    },
-    {
-      id: 2,
-      fullname: "Amir Ali Mahmoudi",
-    },
-  ],
-};
-
-export const fetchUsers = createAsyncThunk("fetch/users", () => {
-  const res = getAllUsers();
-  return res.data
+export const fetchUsers = createAsyncThunk("fetch/users", async () => {
+  const res = await getAllUsers();
+  return res.data;
 });
 
 const userSlice = createSlice({
   name: "user",
-  initialState: initialState,
+  initialState: [],
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export default userSlice.reducer;
