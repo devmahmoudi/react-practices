c17ea6952a299c7ba9b83c0513f90adabc07ea9b
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-26 18:20:38 +0330

    feat(main): dispatch the fetchUsers thunk

--- redux-blog/src/main.jsx
+++ redux-blog/src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import { routes } from './routes'
-import { Provider } from 'react-redux'
-import store from './store'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { routes } from "./routes";
+import { Provider } from "react-redux";
+import store from "./store";
+import { fetchUsers } from "./features/userSlice";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={routes}/>
-    </Provider>
-  </StrictMode>,
-)
+const main = () => {
+  store.dispatch(fetchUsers())
+
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
+    </StrictMode>
+  );
+};
+
+main();
