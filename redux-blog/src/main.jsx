bef779ce3301cd0335204e24a0e6736d626559bd
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-27 18:34:57 +0330

    refactor: move dispatch of fetchUsers thunk from main.jsx to store/index.js

--- redux-blog/src/main.jsx
+++ redux-blog/src/main.jsx
@@ -5,18 +5,11 @@ import "./index.css";
 import { routes } from "./routes";
 import { Provider } from "react-redux";
 import store from "./store";
-import { fetchUsers } from "./features/userSlice";
 
-const main = () => {
-  store.dispatch(fetchUsers())
-
-  createRoot(document.getElementById("root")).render(
-    <StrictMode>
-      <Provider store={store}>
-        <RouterProvider router={routes} />
-      </Provider>
-    </StrictMode>
-  );
-};
-
-main();
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  </StrictMode>
+);
