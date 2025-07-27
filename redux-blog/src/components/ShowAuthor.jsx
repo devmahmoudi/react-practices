c768688a273d264b98cbec5ec233c33333669e31
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-27 18:41:31 +0330

    refactor(ShowAuthor): display 'نویسنده ناشناس' when no user matches blog's user ID

--- redux-blog/src/components/ShowAuthor.jsx
+++ redux-blog/src/components/ShowAuthor.jsx
@@ -4,7 +4,7 @@ import { userSelector } from "../features/userSlice";
 const ShowAuthor = ({ userId }) => {
   const author = useSelector((state) => userSelector(state, userId));
 
-  return <small>{`توسط ${author.fullname}`}</small>;
+  return <small>{`توسط ${author?.fullname ?? "نویسنده ناشناس"}`}</small>;
 };
 
 export default ShowAuthor;
