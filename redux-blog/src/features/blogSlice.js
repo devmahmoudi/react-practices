581a0a498ef2895082f645e067b89395ea0dfeab
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-28 18:12:58 +0330

    feat(blogSlice): set slice status to success on fetch/blogs/fulfilled

--- redux-blog/src/features/blogSlice.js
+++ redux-blog/src/features/blogSlice.js
@@ -1,16 +1,16 @@
 import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
-import { getAllBlogs } from "../services/blogService"
+import { getAllBlogs } from "../services/blogService";
 
 const initialState = {
   blogs: [],
-  status: 'idle',
-  error: null
+  status: "idle",
+  error: null,
 };
 
-export const fetchBlogs = createAsyncThunk('fetch/blogs', async () => {
-  const response = await getAllBlogs()
-  return response.data
-})
+export const fetchBlogs = createAsyncThunk("fetch/blogs", async () => {
+  const response = await getAllBlogs();
+  return response.data;
+});
 
 const blogSlice = createSlice({
   name: "blog",
@@ -18,15 +18,16 @@ const blogSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchBlogs.pending, (state, action) => {
-        state.status = 'pending'
+        state.status = "pending";
       })
       .addCase(fetchBlogs.fulfilled, (state, action) => {
-        state.blogs = action.payload
+        state.status = "success";
+        state.blogs = action.payload;
       })
       .addCase(fetchBlogs.rejected, (state, action) => {
-        state.status = 'error'
-        state.error = action.error.message
-      })
+        state.status = "error";
+        state.error = action.error.message;
+      });
   },
   reducers: {
     blogAdded: {
@@ -72,12 +73,14 @@ const blogSlice = createSlice({
 
 export default blogSlice.reducer;
 
-export const blogSliceStatusSelector = (state) => state.blogs.status
+export const blogSliceStatusSelector = (state) => state.blogs.status;
 
-export const allBlogsSelector = (state) => state.blogs.blogs
+export const blogSliceErrorSelector = (state) => state.blogs.error;
+
+export const allBlogsSelector = (state) => state.blogs.blogs;
 
 export const blogSelector = (state, blogId) =>
-  state.blogs.blogs.find((blog) => blog.id == blogId)
+  state.blogs.blogs.find((blog) => blog.id == blogId);
 
 export const { blogAdded, blogUpdated, blogDeleted, reactionIncrement } =
   blogSlice.actions;
