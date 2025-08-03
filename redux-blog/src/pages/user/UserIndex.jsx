ace8b46ff4ab56aacf23419e5ac19759e6767190
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-03 19:00:57 +0330

    feat(UserIndex): implement user fetching and display in a table format

--- redux-blog/src/pages/user/UserIndex.jsx
+++ redux-blog/src/pages/user/UserIndex.jsx
@@ -1,9 +1,35 @@
-const UserIndex = () => {
+import { useEffect } from "react";
+import { allUsersSelector } from "../../features/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "../../features/userSlice";
 
+const UserIndex = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    const users = useSelector((state) => allUsersSelector(state))
 
     return (
-        <h1>Users index page</h1>    
-    )
+        <table className="rtl-table">  {/* Add class */}
+            <thead>
+                <tr>
+                    <th>آیدی</th>
+                    <th>نام</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.fullname}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 }
 
 export default UserIndex;
