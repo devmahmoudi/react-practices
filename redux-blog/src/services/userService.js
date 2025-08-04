515770022ddff931424973de2aec335e0011ca0d
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-04 16:57:30 +0330

    chore(userService): update route documentation for user fetching and storing

--- redux-blog/src/services/userService.js
+++ redux-blog/src/services/userService.js
@@ -3,12 +3,14 @@ import axios from "axios";
 const SERVER_URL = "http://localhost:1011";
 
 // @desc fetch all users
-// @route http://localhost:9000/users
+// @route GET http://localhost:9000/users
 export const getAllUsers = async () => {
   const url = `${SERVER_URL}/users`;
   return axios.get(url);
 };
 
+// @desc store new user
+// @route POST http://localhost:9000/users
 export const storeUser = async (fullname) => {
   const url = `${SERVER_URL}/users`;
   try {
