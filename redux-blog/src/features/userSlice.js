9ec7a62cfe95c4e76537b3b1c2d56c9a9120eeaa
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-04 16:47:37 +0330

    feat(userSlice): add createUser async action for user creation

--- redux-blog/src/features/userSlice.js
+++ redux-blog/src/features/userSlice.js
@@ -1,22 +1,38 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getAllUsers } from "../services/userService";
+import { getAllUsers, storeUser } from "../services/userService";
 
+// Async actions
 export const fetchUsers = createAsyncThunk("fetch/users", async () => {
   const res = await getAllUsers();
   return res.data;
 });
 
+export const createUser = createAsyncThunk("create/user", async (fullname) => {
+  const res = storeUser(fullname);
+
+  if (res) return res.data;
+});
+
+// User slice object
 const userSlice = createSlice({
   name: "user",
   initialState: [],
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      return action.payload;
-    });
+    builder
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.push(action.payload);
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        console.error("Failed to create user:", action.error.message);
+      });
   },
 });
 
+// exports
 export default userSlice.reducer;
 
 export const userSelector = (state, userId) =>
