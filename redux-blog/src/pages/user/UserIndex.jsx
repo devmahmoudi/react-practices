fe2e6f537e84d65d38b6b92e3c16b946e7096d1c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-04 16:48:17 +0330

    feat(UserIndex): add CreateUser component for new user creation

--- redux-blog/src/pages/user/UserIndex.jsx
+++ redux-blog/src/pages/user/UserIndex.jsx
@@ -1,34 +1,44 @@
 import { useEffect } from "react";
 import { allUsersSelector } from "../../features/userSlice";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchUsers } from "../../features/userSlice";
+import { fetchUsers, createUser } from "../../features/userSlice";
 import { Link } from "react-router-dom";
+import CreateUser from "../../components/user/CreateUser";
 
 const UserIndex = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const users = useSelector((state) => allUsersSelector(state))
+  const users = useSelector((state) => allUsersSelector(state));
 
-    return (
-        <table className="rtl-table">  {/* Add class */}
-            <thead>
-                <tr>
-                    <th>آیدی</th>
-                    <th>نام</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map(user => (
-                    <tr key={user.id}>
-                        <td>{user.id}</td>
-                        <td>
-                            <Link to={`/blogs?userId=${user.id}`}>{user.fullname}</Link>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-}
+  const handleCreateNewUser = fullname => {
+    dispatch(createUser(fullname));
+  }
+
+  return (
+    <>
+      <CreateUser onSubmit={handleCreateNewUser}/>
+      <table className="rtl-table">
+        {" "}
+        {/* Add class */}
+        <thead>
+          <tr>
+            <th>آیدی</th>
+            <th>نام</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>
+                <Link to={`/blogs?userId=${user.id}`}>{user.fullname}</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default UserIndex;
