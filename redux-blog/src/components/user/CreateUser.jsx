23444108018b737c3434323b0c9eb46addd2576c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-05 16:26:40 +0330

    refactor(redux-blog): clearn fullname after create user in CreateUser component

--- redux-blog/src/components/user/CreateUser.jsx
+++ redux-blog/src/components/user/CreateUser.jsx
@@ -5,16 +5,23 @@ const CreateUser = ({ onSubmit }) => {
 
   const canSave = fullname && fullname.trim().length > 0;
 
+  const handleSubmit = () => {
+    onSubmit(fullname);
+
+    setFullname("");
+  };
+
   return (
     <div style={{ margin: 10 }}>
       <label>نام نویسنده جدید</label>
       <input
         type="text"
         name="fullname"
+        value={fullname}
         onChange={(e) => setFullname(e.target.value)}
         placeholder="لطفا نام نویسنده جدید را وارد کنید ..."
       />
-      <button disabled={!canSave} onClick={() => onSubmit(fullname)}>
+      <button disabled={!canSave} onClick={handleSubmit}>
         ذخیره
       </button>
     </div>
