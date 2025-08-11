290fd2d0d858462f9a4c3a42aaf1f07268678f8b
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-11 17:22:35 +0330

    feat(store): integrate apiSlice for enhanced data fetching and middleware support

--- redux-blog/src/store/index.js
+++ redux-blog/src/store/index.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blogReducers from "../features/blogSlice";
 import userReducers, { fetchUsers } from "../features/userSlice";
+import apiSlice from "../api/apiSlice"
 
 const store = configureStore({
   reducer: {
     blogs: blogReducers,
     users: userReducers,
+    [apiSlice.reducerPath]: apiSlice.reducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
 store.dispatch(fetchUsers())
