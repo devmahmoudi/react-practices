b1e54212a9af1a4a80163a9d7c5028d8cd2f9a13
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-16 16:48:23 +0330

    refactor(apiSlice): improve tag management for blog endpoints to enhance caching and invalidation

--- redux-blog/src/api/apiSlice.js
+++ redux-blog/src/api/apiSlice.js
@@ -1,39 +1,49 @@
 // interaction with server by using RTK Query
 
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const apiSlice = createApi({
-    reducerPath: "api", // default state.api
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1011' }),
-    tagTypes: ['BLOG'],
-    endpoints: builder => ({
-        getBlogs: builder.query({
-            query: () => '/blogs',
-            providesTags: ['BLOG']
-        }),
-        getBlog: builder.query({
-            query: (blogId) => `/blogs/${blogId}`
-        }),
-        addNewBlog: builder.mutation({
-            query: (data) => ({
-                url: 'blogs',
-                method: 'POST',
-                body: data
-            }),
-            invalidatesTags: ['BLOG']
-        }),
-        updateBlog: builder.mutation({
-            query: (data) => ({
-                url: `/blogs/${data.id}`,
-                method: 'PUT',
-                body: data
-            }),
-            invalidatesTags: ['BLOG']
-        })
-    })
-}
-)
+  reducerPath: "api", // default state.api
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:1011" }),
+  tagTypes: ["BLOG"],
+  endpoints: (builder) => ({
+    getBlogs: builder.query({
+      query: () => "/blogs",
+      providesTags: (result = [], err, arg) => [
+        "BLOG",
+        ...result.map(({ id }) => ({
+          type: "BLOG", id
+        })),
+      ],
+    }),
+    getBlog: builder.query({
+      query: (blogId) => `/blogs/${blogId}`,
+      providesTags: (result, err, blogId) => [{ type: "BLOG", id: blogId }],
+    }),
+    addNewBlog: builder.mutation({
+      query: (data) => ({
+        url: "blogs",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["BLOG"],
+    }),
+    updateBlog: builder.mutation({
+      query: (data) => ({
+        url: `/blogs/${data.id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: (result, err, blog) => [{ type: "BLOG", id: blog.id }],
+    }),
+  }),
+});
 
-export const { useGetBlogsQuery, useGetBlogQuery, useAddNewBlogMutation, useUpdateBlogMutation } = apiSlice;
+export const {
+  useGetBlogsQuery,
+  useGetBlogQuery,
+  useAddNewBlogMutation,
+  useUpdateBlogMutation,
+} = apiSlice;
 
 export default apiSlice;
