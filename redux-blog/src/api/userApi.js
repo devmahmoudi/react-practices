ff6ea47db1b974bb5f40156fe24396cfa8e0b9ec
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-24 16:51:48 +0330

    feat(user): add comprehensive CRUD operations to user API
    - Added getUser query for fetching single user by ID
    - Implemented createUser mutation for user creation
    - Added updateUser mutation for user updates
    - Implemented deleteUser mutation for user deletion
    - Configured proper cache tags for all endpoints
    - Fixed template literal syntax in deleteUser endpoint URL

--- redux-blog/src/api/userApi.js
+++ redux-blog/src/api/userApi.js
@@ -1,13 +1,44 @@
 import apiSlice from "./apiSlice";
 
 // apis
-export const extenedApi = apiSlice.injectEndpoints({
+export default apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getUsers : builder.query({
+    getUsers: builder.query({
       query: () => "/users",
-      transformResponse: data => {
-        return userAdapter.setAll(initialState, data)
-      }
-    })  
-  })
-})
+      providesTags: (result, error, id) =>
+        result
+          ? [
+              { type: "USER" },
+              ...result.map(({ id }) => ({ type: "USER", id })),
+            ]
+          : ["USER"],
+    }),
+    getUser: builder.query({
+      query: (id) => `/users/${id}`,
+      providesTags: (result, error, id) => [{ type: "USER", id }],
+    }),
+    createUser: builder.mutation({
+      query: (data) => ({
+        url: "/users",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["USER"],
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `/users/${data.id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: (result, error, data) => [{ type: "USER", id: data.id }],
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: $`/users/${userId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (_, __, id) => [{ type: "USER", id }],
+    }),
+  }),
+});
