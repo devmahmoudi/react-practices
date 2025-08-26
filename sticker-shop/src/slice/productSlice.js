aa2deccf0bee55e48d91f49b2a4e11a5c81c2a2f
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-26 17:05:21 +0330

    feat: add productSlice with setAll reducer

---
+++ sticker-shop/src/slice/productSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import productApi from "../api/productApi"
+
+const productAdapter = createEntityAdapter()
+
+const initialState = productAdapter.getInitialState({
+    status: "idle"
+})
+
+const productSlice = createSlice({
+    initialState: initialState,
+    extraReducers: (builder) => {
+        builder.addMatcher(productApi.endpoints.getProducts.matchFulfilled, productAdapter.setAll)
+    }
+});
+
+export default productSlice;
