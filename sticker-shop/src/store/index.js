9ee7af1368978b293b575d914d4b06acb9f3ba13
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-27 16:28:06 +0330

    feat(store): initialize Redux store with product and API slices

---
+++ sticker-shop/src/store/index.js
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productSlice from "../slice/productSlice";
+import apiSlice from "../api/apiSlice";
+import productApi from "../api/productApi";
+
+export const store = configureStore({
+  reducer: {
+    products: productSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefault) => {
+    return getDefault().concat(apiSlice.middleware);
+  },
+});
+
+
+store.dispatch(productApi.endpoints.getProducts.initiate())
+
+export default store;
