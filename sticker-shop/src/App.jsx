ef61d9957016ce6b09e90f0987f3096a926eb1ea
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-09-12 17:50:59 +0330

    feat(routing): add NotFound page and update routing to handle unmatched paths

--- sticker-shop/src/App.jsx
+++ sticker-shop/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainLayout from "./components/layouts/MainLayout";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
+      <Route path="*" element={<NotFound/>}/>
+      <Route path="/" element={<MainLayout />} errorElement={<NotFound />}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
---
+++ sticker-shop/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+const NotFound = () => {
+    return (
+        <h1 className="text-xl font-primary">صفحه ای که دنبالش میگردی را پیدا نکردم 😁</h1>
+    )
+}
+
+export default NotFound;
