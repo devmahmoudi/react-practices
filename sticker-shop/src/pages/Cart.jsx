5a6ba9c9abd49b9af954f6c1ad1c666f701ddce4
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-09-16 18:25:06 +0330

    feat(cart): enhance Cart page with item listing, total calculation, and table display

--- sticker-shop/src/pages/Cart.jsx
+++ sticker-shop/src/pages/Cart.jsx
@@ -1,7 +1,45 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Price from "../components/ui/Price";
+import Table from "../components/ui/Table";
+
 const Cart = () => {
-    return (
-        <h1>Welcome to cartable</h1>
-    )
-}
+
+  const items = Object.values(useSelector((state) => state.cart.entities)) ?? [];
+
+  const cartTotalPrice = useSelector((state) => state.cart.total)
+
+  const cartTotalQuent = items.length ? items.reduce((a, b) => a.quent + b.quent) : 0
+
+  const tableHeaders = ["محصول", "تعداد", "قیمت", "حذف"];
+
+  const tableRows = Object.values(items).map((item) => [
+    item.title,
+    item.quent,
+    item.price,
+    "",
+  ]);
+
+  const tableFooter = [
+    "مجموع",
+    cartTotalQuent,
+    <Price
+      value={cartTotalPrice}
+      displayType="text"
+      thousandSeparator=","
+      suffix=" تومان"
+    />,
+  ];
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="font-primary">
+      <h3 className="text-3xl text-palette-primary mb-5">سبد خرید شما</h3>
+
+      <Table headers={tableHeaders} rows={tableRows} footer={tableFooter} />
+    </div>
+  );
+};
 
 export default Cart;
