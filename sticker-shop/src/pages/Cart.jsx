c282b9253e9c479c72ab04d330fd06ea0254a739
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-09-16 18:33:19 +0330

    refactor(cart): improve Cart component with detailed comments and enhance total quantity calculation

--- sticker-shop/src/pages/Cart.jsx
+++ sticker-shop/src/pages/Cart.jsx
@@ -4,22 +4,47 @@ import Price from "../components/ui/Price";
 import Table from "../components/ui/Table";
 
 const Cart = () => {
+  /**
+   * Select cart items from the cartSlice
+   */
+  const items =
+    Object.values(useSelector((state) => state.cart.entities)) ?? [];
 
-  const items = Object.values(useSelector((state) => state.cart.entities)) ?? [];
+  /**
+   * Select cart total price from the cartSlice
+   */
+  const cartTotalPrice = useSelector((state) => state.cart.total);
 
-  const cartTotalPrice = useSelector((state) => state.cart.total)
+  /**
+   * Calculate total quent of all cart items
+   */
+  const cartTotalQuent = items.length
+    ? items.reduce((a, b) => a + b.quent, 0)
+    : 0;
 
-  const cartTotalQuent = items.length ? items.reduce((a, b) => a.quent + b.quent) : 0
-
+  /**
+   * Cart table headers object
+   */
   const tableHeaders = ["محصول", "تعداد", "قیمت", "حذف"];
 
+  /**
+   * Cart table rows object
+   */
   const tableRows = Object.values(items).map((item) => [
     item.title,
     item.quent,
-    item.price,
+    <Price
+      value={item.price}
+      displayType="text"
+      thousandSeparator=","
+      suffix=" تومان"
+    />,
     "",
   ]);
 
+  /**
+   * Cart table footer object
+   */
   const tableFooter = [
     "مجموع",
     cartTotalQuent,
@@ -30,8 +55,6 @@ const Cart = () => {
       suffix=" تومان"
     />,
   ];
-
-  useEffect(() => {}, []);
 
   return (
     <div className="font-primary">
