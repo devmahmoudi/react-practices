224b3a09a623e2a4924fef7ec585ecff4da66b0c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-09-18 19:34:41 +0330

    refactor(navbar): update cart item count display logic and enhance comments for clarity

--- sticker-shop/src/components/NavBar.jsx
+++ sticker-shop/src/components/NavBar.jsx
@@ -1,7 +1,13 @@
 import { FaCartShopping } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  /**
+   * Select cart items count ( sum of all itesm quent )
+   */
+  const cartItemsCount = useSelector((state) => state.cart.count);
+
   return (
     <nav className="border-b border-palette-lighter sticky top-0 z-20 bg-gray-400 rounded-lg text-palette-primary">
       <div className="flex items-center justify-between w-full p-5">
@@ -14,16 +20,12 @@ const Navbar = () => {
         </Link>
         <div>
           <Link to="/cart" className="relative">
-            <FaCartShopping size={25} className="text-pallete-primary"/>
-            {/* {cartItems.length === 0 ? null : ( */}
-            <div className="absolute top-0 right-0 text-xs bg-yellow-300 text-gray-900 font-semibold rounded-full p-1 transform translate-x-6 -translate-y-3 text-[10px]">
-              {/* <CustomNumeralNumericFormat
-                                    value={cartItems.length}
-                                    thousandSeparator=","
-                                /> */}
-              10
-            </div>
-            {/* )} */}
+            <FaCartShopping size={25} className="text-pallete-primary" />
+            {cartItemsCount > 0 && (
+              <div className="absolute top-0 right-0 text-xs bg-yellow-300 text-gray-900 font-semibold rounded-full p-1 transform translate-x-6 -translate-y-3 text-[10px]">
+                {cartItemsCount}
+              </div>
+            )}
           </Link>
         </div>
       </div>
