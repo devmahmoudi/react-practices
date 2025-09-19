821b62628e88a0e4ed484356c3dce9efaf7a6d4c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-09-19 16:50:24 +0330

    feat(product-list): implement pagination for product listing with Paginator component integration

--- sticker-shop/src/components/product/ProductList.jsx
+++ sticker-shop/src/components/product/ProductList.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
 import productApi from "../../api/productApi";
+import { Paginator } from "../ui/Paginator";
 import ProductCard from "./ProductCard";
+import { useEffect } from "react";
 
 const ProductList = () => {
+  /**
+   * RTK Query fetching list
+   */
   const { data, isLoading, isSuccess, isError, error } =
     productApi.useGetProductsQuery();
+
+  /**
+   * Paginator par page items length
+   */
+  const [perPage, setPerPage] = useState(12);
+
+  /**
+   * Paginator current page items
+   */
+  const [currentItems, setCurrentItems] = useState([]);
+
+  useEffect(() => {
+    if (!currentItems.length && isSuccess && data)
+      setCurrentItems(data.slice(0, perPage));
+  }, [data]);
+
+  /**
+   * Paginator on page change handler
+   *
+   * @param {number} offset
+   * @param {number} end
+   */
+  const handlePageChange = (offset, end) => {
+    setCurrentItems(data.slice(offset, end));
+  };
 
   const content = () => {
     if (isLoading) return <span>در حال بارگزاری ...</span>;
@@ -14,18 +45,27 @@ const ProductList = () => {
       else
         return (
           <>
-            {data.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+            <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 font-primary">
+              {currentItems.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </div>
           </>
         );
     }
   };
 
   return (
-    <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 font-primary">
-        {content()}
-    </div>
+    <>
+      {content()}
+      {data && data.length > perPage && (
+        <Paginator
+          total={data.length}
+          onPageChange={handlePageChange}
+          perPage={perPage}
+        />
+      )}
+    </>
   );
 };
 
