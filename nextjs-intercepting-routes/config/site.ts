f9e3a4564f485898e693b996092f43531a632f20
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-25 19:12:56 +0330

    refactor: update site configuration and clean up unused components in the navigation and header

--- nextjs-intercepting-routes/config/site.ts
+++ nextjs-intercepting-routes/config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Gallery Application",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Beautifully designed gallery with view image in modal using the NextJs intercepting routes",
   mainNav: [
     {
       title: "Home",
