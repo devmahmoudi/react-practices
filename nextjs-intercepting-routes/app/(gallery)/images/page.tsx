f7e9b165046f3ccb0cf3d6c5eb9c066b08caab76
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-26 19:33:07 +0330

    feat(images): create Images component to display a grid of images from the data module

---
+++ nextjs-intercepting-routes/app/(gallery)/images/page.tsx
@@ -0,0 +1,17 @@
+import { images } from "./data"
+
+export default function Images() {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {images.map((image) => (
+        <div className="p-2">
+          <img
+            key={image.id}
+            src={image.url}
+            className="w-full h-[250px] dark:border border-dark-400 rounded-lg cursor-pointer hover:scale-105 transition-all duration-300"
+          />
+        </div>
+      ))}
+    </div>
+  )
+}
