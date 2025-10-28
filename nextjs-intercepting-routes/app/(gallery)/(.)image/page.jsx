2418a6b4fc72920a68d6f27acce7dadc3cd81f81
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-28 20:25:16 +0330

    chore(images): remove unused image components and data module to clean up the gallery structure

--- nextjs-intercepting-routes/app/(gallery)/(.)image/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function ImageModal() {
-  return <h1>That's it, Congratulation !</h1>
-}
--- nextjs-intercepting-routes/app/(gallery)/image/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function ImageDetailModal() {
-  return <h1>This is image detail page !</h1>
-}
