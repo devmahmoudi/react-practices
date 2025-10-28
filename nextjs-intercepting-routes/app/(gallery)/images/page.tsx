9509835eb956e038e0f6d8e34847b24d0eb65b1c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-28 19:28:40 +0330

    feat(images): wrap images in Link component for navigation to individual image pages

--- nextjs-intercepting-routes/app/(gallery)/images/page.tsx
+++ nextjs-intercepting-routes/app/(gallery)/images/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 import { images } from "./data"
 
 export default function Images() {
@@ -5,11 +7,13 @@ export default function Images() {
     <div className="grid grid-cols-3 gap-4">
       {images.map((image) => (
         <div className="p-2">
-          <img
-            key={image.id}
-            src={image.url}
-            className="w-full h-[250px] dark:border border-dark-400 rounded-lg cursor-pointer hover:scale-105 transition-all duration-300"
-          />
+          <Link href={'/image'}>
+            <img
+              key={image.id}
+              src={image.url}
+              className="w-full h-[250px] dark:border border-dark-400 rounded-lg cursor-pointer hover:scale-105 transition-all duration-300"
+            />
+          </Link>
         </div>
       ))}
     </div>
