59a78854a9ff7339ba263d5650eaf8cce0b72f97
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-02 19:27:01 +0330

    chore(site): clear main navigation array to simplify site configuration

--- nextjs-intercepting-routes/config/site.ts
+++ nextjs-intercepting-routes/config/site.ts
@@ -4,12 +4,7 @@ export const siteConfig = {
   name: "Gallery Application",
   description:
     "Beautifully designed gallery with view image in modal using the NextJs intercepting routes",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
+  mainNav: [],
   links: {
     twitter: "https://twitter.com/shadcn",
     github: "https://github.com/shadcn/ui",
