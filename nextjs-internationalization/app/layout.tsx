fa347a5bfc1f8b08473429e7360ddce86e6786d1
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-08 19:03:37 +0330

    fix(layout): restore children rendering in RootLayout and update IndexPage content for clarity

--- nextjs-internationalization/app/layout.tsx
+++ nextjs-internationalization/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <AppSidebar />
               <main>
                 <SidebarTrigger />
-                {/* {children} */}
+                {children}
               </main>
             </SidebarProvider>
             <TailwindIndicator />
--- nextjs-internationalization/app/page.tsx
+++ nextjs-internationalization/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { Layers, SquareLibrary } from "lucide-react"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
@@ -8,12 +9,11 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          NextJs Internationalization & Route Handler
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          This is a practice project wrote with Mahdi Mahmoudi <br />
+          for learning about NextJs Internationaliztion and Route Handler
         </p>
       </div>
       <div className="flex gap-4">
@@ -21,9 +21,10 @@ export default function IndexPage() {
           href={siteConfig.links.docs}
           target="_blank"
           rel="noreferrer"
-          className={buttonVariants()}
+          className={buttonVariants({ variant: "outline" })}
         >
-          Documentation
+          <SquareLibrary className="pr-3" />
+          Categories
         </Link>
         <Link
           target="_blank"
@@ -31,7 +32,8 @@ export default function IndexPage() {
           href={siteConfig.links.github}
           className={buttonVariants({ variant: "outline" })}
         >
-          GitHub
+          <Layers className="pr-3" />
+          Blogs
         </Link>
       </div>
     </section>
