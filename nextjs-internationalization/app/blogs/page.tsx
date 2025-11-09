3d7e6896d785c758e84c5834aa9525b55f99b26d
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-09 19:15:01 +0330

    feat: add shadcn table component in use it in /blogs page

--- nextjs-internationalization/app/blogs/page.tsx
+++ nextjs-internationalization/app/blogs/page.tsx
@@ -1,15 +1,40 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 export default function BlogsPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container grid w-full items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Blogs
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Blogs page content
-        </p>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </section>
   )
 }
-
