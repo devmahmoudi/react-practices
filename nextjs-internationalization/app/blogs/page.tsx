69bd7c541ed5e874ef1c0d24a39bf8879889e3b1
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-09 20:12:55 +0330

    feat(blogs): implement blog listing with data fetching and dynamic rendering

--- nextjs-internationalization/app/blogs/page.tsx
+++ nextjs-internationalization/app/blogs/page.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+import Link from "next/link"
+import { useGetBlogsQuery } from "@/store/api"
+
 import {
   Table,
   TableBody,
@@ -9,29 +14,38 @@ import {
 } from "@/components/ui/table"
 
 export default function BlogsPage() {
+  const { data, error, isLoading } = useGetBlogsQuery()
+
+  if (isLoading) return <p className="text-center">Loading Blogs ...</p>
+
   return (
-    <section className="container grid w-full items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container grid w-full items-center gap-6 pb-8 md:py-2">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Blogs
         </h1>
         <Table>
-          <TableCaption>A list of your recent invoices.</TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
+              <TableHead className="w-[100px]">ID</TableHead>
+              <TableHead>Title</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-              <TableCell className="text-right">$250.00</TableCell>
-            </TableRow>
+            {data &&
+              data.length &&
+              data.map((blog) => (
+                <TableRow>
+                  <TableCell className="font-medium">{blog.id}</TableCell>
+                  <TableCell>{blog.title}</TableCell>
+                  <TableCell>{blog.category_id}</TableCell>
+                  <TableCell className="text-right">
+                    <Link href="/">Edit</Link>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </div>
