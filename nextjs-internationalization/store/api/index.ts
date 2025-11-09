dec1a2976f2eaac6d2fc94b7f8c5bec45edb1c28
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-09 20:06:03 +0330

    fix(api): update endpoint for fetching blogs and standardize import statements

--- nextjs-internationalization/store/api/index.ts
+++ nextjs-internationalization/store/api/index.ts
@@ -1,13 +1,13 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import type { Blog } from '@/types'
+import type { Blog } from "@/types"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3010' }),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3010" }),
   endpoints: (build) => ({
     getBlogs: build.query<Blog, void>({
-      query: () => `/`,
+      query: () => `/blogs`,
     }),
   }),
 })
