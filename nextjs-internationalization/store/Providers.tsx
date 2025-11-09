611fb9fb469699b17bf43c2f3f037cfa92d3d80c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-09 22:05:21 +0330

    feat(store): prefetch categories on app load and enhance category slice with selectors

--- nextjs-internationalization/store/Providers.tsx
+++ nextjs-internationalization/store/Providers.tsx
@@ -1,8 +1,15 @@
-'use client'
+"use client"
 
-import { Provider } from 'react-redux'
-import { store } from '@/store'
+import { useEffect } from "react"
+import { store } from "@/store"
+import { categoryApi } from "@/store/api/categoryApi"
+import { Provider } from "react-redux"
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Prefetch categories when the app loads
+    store.dispatch(categoryApi.endpoints.getCategories.initiate())
+  }, [])
+
   return <Provider store={store}>{children}</Provider>
 }
--- nextjs-internationalization/store/slice/categorySlice.ts
+++ nextjs-internationalization/store/slice/categorySlice.ts
@@ -1,16 +1,60 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import {
+  PayloadAction,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit"
 
-const initialState = {}
+import { categoryApi } from "../api/categoryApi"
+import type { RootState } from "../index"
+import type { Category } from "@/types"
 
+/**
+ * Init category adapter
+ */
+const categoryAdapter = createEntityAdapter<Category>()
+
+/**
+ * Define inital state mutable
+ */
+const initialState = categoryAdapter.getInitialState()
+
+/**
+ * Category slice
+ */
 export const CategorySlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState,
   reducers: {
     //
-  }
+  },
+  extraReducers: (builder) => {
+    /**
+     * Set all categories on getCategories api fulfidlled
+     */
+    builder.addMatcher(
+      categoryApi.endpoints.getCategories.matchFulfilled,
+      (state, { payload }) => categoryAdapter.setAll(state, payload)
+    )
+  },
 })
 
 // Action creators are generated for each case reducer function
 // export const {  } = CategorySlice.actions
 
 export default CategorySlice.reducer
+
+/**
+ * Category selectors
+ */
+export const categorySelectors = categoryAdapter.getSelectors(
+  (state: RootState) => state.category
+)
+
+// Export individual selectors for convenience
+export const {
+  selectById: selectCategoryById,
+  selectAll: selectAllCategories,
+  selectIds: selectCategoryIds,
+  selectEntities: selectCategoryEntities,
+  selectTotal: selectCategoryTotal,
+} = categorySelectors
