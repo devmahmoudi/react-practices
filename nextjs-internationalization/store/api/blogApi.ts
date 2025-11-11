80cb5f5d0aae010792c5952ed24b09395c2bdd9f
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-11 19:31:36 +0330

    feat(api): add tagTypes for BLOG and CATEGORY to enhance cache management in blogApi

--- nextjs-internationalization/store/api/blogApi.ts
+++ nextjs-internationalization/store/api/blogApi.ts
@@ -1,10 +1,15 @@
 import type { Blog } from "@/types"
+
 import { baseApi } from "."
 
 const blogApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getBlogs: build.query<Blog[], void>({
       query: () => `/blogs`,
+      providesTags: (result, error, arg) =>
+        result
+          ? [...result.map(({ id }) => ({ type: "BLOG" as const, id })), "BLOG"]
+          : ["BLOG"],
     }),
   }),
 })
--- nextjs-internationalization/store/api/index.ts
+++ nextjs-internationalization/store/api/index.ts
@@ -4,5 +4,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3010" }),
-  endpoints: () => ({})
+  endpoints: () => ({}),
+  tagTypes: ['BLOG', 'CATEGORY']
 })
