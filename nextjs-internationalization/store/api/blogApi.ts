a6a0c66c145482994edd19534dfea30ff83a0de9
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-11 20:36:26 +0330

    feat(api): add mutation for creating new blog posts in blogApi

--- nextjs-internationalization/store/api/blogApi.ts
+++ nextjs-internationalization/store/api/blogApi.ts
@@ -11,7 +11,15 @@ const blogApi = baseApi.injectEndpoints({
           ? [...result.map(({ id }) => ({ type: "BLOG" as const, id })), "BLOG"]
           : ["BLOG"],
     }),
+    addBlog: build.mutation<Blog, Omit<Blog, 'id'>>({
+      query: (body) => ({
+        url: '/blogs',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['BLOG']
+    }),
   }),
 })
 
-export const { useGetBlogsQuery } = blogApi
+export const { useGetBlogsQuery, useAddBlogMutation } = blogApi
