f5e854698a6a76de897315b10cebd2858634525f
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-13 20:31:47 +0330

    feat(categories): enhance categories page with listing, deletion functionality, and loading state

--- nextjs-internationalization/app/categories/page.tsx
+++ nextjs-internationalization/app/categories/page.tsx
@@ -1,15 +1,104 @@
+"use client"
+
+import Link from "next/link"
+import {
+  useDeleteCategoryMutation,
+  useGetCategoriesQuery,
+} from "@/store/api/categoryApi"
+import { CirclePlus } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { showConfirmDialog } from "@/components/confirm-dialog"
+
 export default function CategoriesPage() {
+  /**
+   * Get categories list
+   */
+  const { data, isLoading } = useGetCategoriesQuery()
+
+  /**
+   * Delete category
+   */
+  const [deleteCategory, { isLoading: isDeleting }] =
+    useDeleteCategoryMutation()
+
+  /**
+   * Delete category button on click handler
+   */
+  const deleteCategoryHandler = async (categoryId: number) => {
+    const confirmed = await showConfirmDialog({
+      title: `Delete Category ${categoryId}`,
+      message:
+        "Are you sure you want to delete this category? This action cannot be undone.",
+      confirmText: "Delete",
+      cancelText: "Cancel",
+    })
+    if (confirmed) {
+      deleteCategory(categoryId)
+    }
+  }
+
+  /**
+   * Loading state
+   */
+  if (isLoading) return <p className="text-center">Loading Categories...</p>
+
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container grid w-full items-center gap-6 pb-8 md:py-2">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Categories
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Categories page content
-        </p>
+        <div className="flex justify-between align-middle w-full mb-3">
+          <h3 className="text-2xl font-extrabold leading-tight tracking-tighter md:text-3xl">
+            Categories
+          </h3>
+          <Button className="pl-2">
+            <Link href={"/categories/create"} className="flex align-middle">
+              <CirclePlus className="pr-2 box-content" />
+              <span className="pt-[2px]">Add New Category</span>
+            </Link>
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">ID</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data &&
+              data.length &&
+              data.map((category) => (
+                <TableRow key={category.id}>
+                  <TableCell className="font-medium">{category.id}</TableCell>
+                  <TableCell>{category.name}</TableCell>
+                  <TableCell className="text-right">
+                    <Link
+                      href={`/categories/edit/${category.id}`}
+                      className="text-blue-400"
+                    >
+                      Edit
+                    </Link>
+                    <span
+                      className="ml-3 text-red-400 cursor-pointer"
+                      onClick={() => deleteCategoryHandler(category.id)}
+                    >
+                      Delete
+                    </span>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
       </div>
     </section>
   )
 }
-
