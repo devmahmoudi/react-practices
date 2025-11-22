824193544d3de76414c137ea93a81b78274138aa
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-22 20:14:39 +0330

    feat(categories): implement CategoriesClient component for category management with delete functionality

---
+++ nextjs-internationalization/app/[lang]/categories/CategoriesClient.tsx
@@ -0,0 +1,115 @@
+"use client"
+
+import React from "react"
+import Link from "next/link"
+import {
+  useDeleteCategoryMutation,
+  useGetCategoriesQuery,
+} from "@/store/api/categoryApi"
+import { CirclePlus } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { showConfirmDialog } from "@/components/confirm-dialog"
+
+type Dictorinary = object
+
+type Props = {
+  dictionary: Dictorinary
+  lang: string
+}
+
+export default function CategoriesClient({ dictionary }: Props) {
+  /**
+   * Parse dictionary prop to object
+   */
+  dictionary = JSON.parse(dictionary.value)
+
+  /**
+   * Get categories list
+   */
+  const { data, isLoading } = useGetCategoriesQuery()
+
+  /**
+   * Delete category
+   */
+  const [deleteCategory, { isLoading: isDeleting }] =
+    useDeleteCategoryMutation()
+
+  /**
+   * Delete category button on click handler
+   */
+  const deleteCategoryHandler = async (categoryId: number) => {
+    const confirmed = await showConfirmDialog({
+      title: `Delete Category ${categoryId}`,
+      message:
+        "Are you sure you want to delete this category? This action cannot be undone.",
+      confirmText: "Delete",
+      cancelText: "Cancel",
+    })
+    if (confirmed) {
+      deleteCategory(categoryId)
+    }
+  }
+
+  /**
+   * Loading state
+   */
+  if (isLoading) return <p className="text-center">Loading Categories...</p>
+
+  return (
+    <section className="container grid w-full items-center gap-6 pb-8 md:py-2">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <div className="flex justify-between align-middle w-full mb-3">
+          <h3 className="text-2xl font-extrabold leading-tight tracking-tighter md:text-3xl">
+            {dictionary.categories.index.title}
+          </h3>
+          <Button className="pl-2">
+            <Link href={"/categories/create"} className="flex align-middle">
+              <CirclePlus className="pr-2 box-content" />
+              <span className="pt-[2px]">Add New Category</span>
+            </Link>
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data &&
+              data.length &&
+              data.map((category) => (
+                <TableRow key={category.id}>
+                  <TableCell>{category.name}</TableCell>
+                  <TableCell className="text-right">
+                    <Link
+                      href={`/categories/edit/${category.id}`}
+                      className="text-blue-400"
+                    >
+                      Edit
+                    </Link>
+                    <span
+                      className="ml-3 text-red-400 cursor-pointer"
+                      onClick={() => deleteCategoryHandler(category.id)}
+                    >
+                      Delete
+                    </span>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+    </section>
+  )
+}
--- nextjs-internationalization/app/[lang]/categories/page.tsx
+++ nextjs-internationalization/app/[lang]/categories/page.tsx
@@ -1,102 +1,24 @@
-"use client"
+import React from "react"
+import CategoriesClient from "./CategoriesClient"
+import { getDictionary } from "../localization/dictrionaries"
 
-import Link from "next/link"
-import {
-  useDeleteCategoryMutation,
-  useGetCategoriesQuery,
-} from "@/store/api/categoryApi"
-import { CirclePlus } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import { showConfirmDialog } from "@/components/confirm-dialog"
-
-export default function CategoriesPage() {
+export default async function CategoriesPage({
+  params,
+}: {
+  params: Promise<{ lang: string }>
+}) {
   /**
-   * Get categories list
+   * Destruct client selected language
    */
-  const { data, isLoading } = useGetCategoriesQuery()
+  const { lang } = await params
 
   /**
-   * Delete category
+   * Load dictionary translation object through lang prop for localization
    */
-  const [deleteCategory, { isLoading: isDeleting }] =
-    useDeleteCategoryMutation()
+  const dictionary = getDictionary(lang)
 
   /**
-   * Delete category button on click handler
+   * Return client component as response
    */
-  const deleteCategoryHandler = async (categoryId: number) => {
-    const confirmed = await showConfirmDialog({
-      title: `Delete Category ${categoryId}`,
-      message:
-        "Are you sure you want to delete this category? This action cannot be undone.",
-      confirmText: "Delete",
-      cancelText: "Cancel",
-    })
-    if (confirmed) {
-      deleteCategory(categoryId)
-    }
-  }
-
-  /**
-   * Loading state
-   */
-  if (isLoading) return <p className="text-center">Loading Categories...</p>
-
-  return (
-    <section className="container grid w-full items-center gap-6 pb-8 md:py-2">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <div className="flex justify-between align-middle w-full mb-3">
-          <h3 className="text-2xl font-extrabold leading-tight tracking-tighter md:text-3xl">
-            Categories
-          </h3>
-          <Button className="pl-2">
-            <Link href={"/categories/create"} className="flex align-middle">
-              <CirclePlus className="pr-2 box-content" />
-              <span className="pt-[2px]">Add New Category</span>
-            </Link>
-          </Button>
-        </div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead className="text-right">Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data &&
-              data.length &&
-              data.map((category) => (
-                <TableRow key={category.id}>
-                  <TableCell>{category.name}</TableCell>
-                  <TableCell className="text-right">
-                    <Link
-                      href={`/categories/edit/${category.id}`}
-                      className="text-blue-400"
-                    >
-                      Edit
-                    </Link>
-                    <span
-                      className="ml-3 text-red-400 cursor-pointer"
-                      onClick={() => deleteCategoryHandler(category.id)}
-                    >
-                      Delete
-                    </span>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </div>
-    </section>
-  )
+  return <CategoriesClient dictionary={dictionary}/>
 }
