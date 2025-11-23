8a6129b87da3f10e546a258eaf11a914eb68c415
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-23 20:45:11 +0330

    refactor(blogs): simplify BlogsPage component by delegating rendering to BlogsClient and enhance localization support

---
+++ nextjs-internationalization/app/[lang]/blogs/BlogsClient.tsx
@@ -0,0 +1,123 @@
+"use client"
+
+import Link from "next/link"
+import type { RootState } from "@/store"
+import { useDeleteBlogMutation, useGetBlogsQuery } from "@/store/api/blogApi"
+import { selectCategoryEntities } from "@/store/slice/categorySlice"
+import type { Category, Dictorinary } from "@/types"
+import { translate } from "@/utils/localization/helper"
+import { CirclePlus } from "lucide-react"
+import { useSelector } from "react-redux"
+
+import { Button } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { showConfirmDialog } from "@/components/confirm-dialog"
+
+export default function BlogsClient({
+  dictionary,
+}: {
+  dictionary: Dictorinary
+}) {
+  /**
+   * Get blogs list
+   */
+  const { data, error, isLoading } = useGetBlogsQuery()
+
+  /**
+   * Delete blog
+   */
+  const [deleteBlog, { isLoading: isDeleting }] = useDeleteBlogMutation()
+
+  /**
+   * Select categories
+   */
+  const categoryEntities = useSelector((state: RootState) =>
+    selectCategoryEntities(state)
+  ) as Record<number, Category>
+
+  /**
+   * Delete blog button on click handler
+   */
+  const deleteBlogHandler = async (blogId: number) => {
+    const confirmed = await showConfirmDialog({
+      title: `Delete Blog Post ${blogId}`,
+      message:
+        "Are you sure you want to delete this blog post? This action cannot be undone.",
+      confirmText: "Delete",
+      cancelText: "Cancel",
+    })
+
+    if (confirmed) deleteBlog(blogId)
+  }
+
+  /**
+   * Loading state
+   */
+  if (isLoading) return <p className="text-center">Loading Blogs ...</p>
+
+  return (
+    <section className="container grid w-full items-center gap-6 pb-8 md:py-2">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <div className="flex justify-between align-middle w-full mb-3">
+          <h3 className="text-2xl font-extrabold leading-tight tracking-tighter md:text-3xl">
+            {translate("Blogs", dictionary)}
+          </h3>
+          <Button className="pl-2">
+            <Link href={"/blogs/create"} className="flex align-middle">
+              <CirclePlus className="pr-2 box-content" />
+              <span className="pt-[2px]">
+                {translate("Draft New Blog", dictionary)}
+              </span>
+            </Link>
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>{translate("Title", dictionary)}</TableHead>
+              <TableHead>{translate("Category", dictionary)}</TableHead>
+              <TableHead className="text-right">
+                {translate("Actions", dictionary)}
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {(data &&
+              data.length &&
+              data.map((blog) => (
+                <TableRow key={blog.id}>
+                  <TableCell>{blog.title}</TableCell>
+                  <TableCell>
+                    {categoryEntities[blog.category_id]?.name || "N/A"}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Link
+                      href={`/blogs/edit/${blog.id}`}
+                      className="text-blue-400"
+                    >
+                      Edit
+                    </Link>
+                    <span
+                      className="ml-3 text-red-400 cursor-pointer"
+                      onClick={() => deleteBlogHandler(blog.id)}
+                    >
+                      Delete
+                    </span>
+                  </TableCell>
+                </TableRow>
+              ))) ||
+              ""}
+          </TableBody>
+        </Table>
+      </div>
+    </section>
+  )
+}
--- nextjs-internationalization/app/[lang]/blogs/page.tsx
+++ nextjs-internationalization/app/[lang]/blogs/page.tsx
@@ -1,114 +1,23 @@
-"use client"
+import { getDictionary } from "@/utils/localization/dictrionaries"
+import BlogsClient from "./BlogsClient"
 
-import Link from "next/link"
-import type { RootState } from "@/store"
-import { useDeleteBlogMutation, useGetBlogsQuery } from "@/store/api/blogApi"
-import { selectCategoryEntities } from "@/store/slice/categorySlice"
-import type { Category } from "@/types"
-import { CirclePlus } from "lucide-react"
-import { useSelector } from "react-redux"
-
-import { Button } from "@/components/ui/button"
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import { showConfirmDialog } from "@/components/confirm-dialog"
-
-export default function BlogsPage() {
+export default async function({
+  params,
+}: {
+  params: Promise<{ lang: string }>
+}) {
   /**
-   * Get blogs list
+   * Destruct client selected language
    */
-  const { data, error, isLoading } = useGetBlogsQuery()
+  const { lang } = await params
 
   /**
-   * Delete blog
+   * Load dictionary translation object through lang prop for localization
    */
-  const [deleteBlog, { isLoading: isDeleting }] = useDeleteBlogMutation()
+  const dictionary = await getDictionary(lang)
 
   /**
-   * Select categories
+   * Return client component as response
    */
-  const categoryEntities = useSelector((state: RootState) =>
-    selectCategoryEntities(state)
-  ) as Record<number, Category>
-
-  /**
-   * Delete blog button on click handler
-   */
-  const deleteBlogHandler = async (blogId: number) => {
-    const confirmed = await showConfirmDialog({
-      title: `Delete Blog Post ${blogId}`,
-      message:
-        "Are you sure you want to delete this blog post? This action cannot be undone.",
-      confirmText: "Delete",
-      cancelText: "Cancel",
-    })
-
-    if (confirmed) deleteBlog(blogId)
-  }
-
-  /**
-   * Loading state
-   */
-  if (isLoading) return <p className="text-center">Loading Blogs ...</p>
-
-  return (
-    <section className="container grid w-full items-center gap-6 pb-8 md:py-2">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <div className="flex justify-between align-middle w-full mb-3">
-          <h3 className="text-2xl font-extrabold leading-tight tracking-tighter md:text-3xl">
-            Blogs
-          </h3>
-          <Button className="pl-2">
-            <Link href={"/blogs/create"} className="flex align-middle">
-              <CirclePlus className="pr-2 box-content" />
-              <span className="pt-[2px]">Draft New Blog</span>
-            </Link>
-          </Button>
-        </div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Title</TableHead>
-              <TableHead>Category</TableHead>
-              <TableHead className="text-right">Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {(data &&
-              data.length &&
-              data.map((blog) => (
-                <TableRow key={blog.id}>
-                  <TableCell>{blog.title}</TableCell>
-                  <TableCell>
-                    {categoryEntities[blog.category_id]?.name || "N/A"}
-                  </TableCell>
-                  <TableCell className="text-right">
-                    <Link
-                      href={`/blogs/edit/${blog.id}`}
-                      className="text-blue-400"
-                    >
-                      Edit
-                    </Link>
-                    <span
-                      className="ml-3 text-red-400 cursor-pointer"
-                      onClick={() => deleteBlogHandler(blog.id)}
-                    >
-                      Delete
-                    </span>
-                  </TableCell>
-                </TableRow>
-              ))) ||
-              ""}
-          </TableBody>
-        </Table>
-      </div>
-    </section>
-  )
+  return <BlogsClient dictionary={dictionary}/>
 }
