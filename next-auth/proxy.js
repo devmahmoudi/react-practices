dc5a7f319c0ec5d924858bf92eebe9ada50ef6f0
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-26 20:19:50 +0330

    feat(auth): add proxy.js with exporting next auth middleware to protect /dashboad routes

--- next-auth/proxy.js
+++ next-auth/proxy.js
@@ -1,1 +1,10 @@
-export { default } from "next-auth/middleware"
+export { default } from "next-auth/middleware";
+
+// This function can be marked `async` if using `await` inside
+// export function proxy(request) {
+//   return NextResponse.next();
+// }
+
+export const config = {
+  matcher: ["/dashboard"],
+};
