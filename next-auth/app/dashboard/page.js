f9809fecebcfaa3435bb9ab12f57592d43d74373
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-26 21:29:09 +0330

    feat(dashboard): integrate server session retrieval for dashboard component

--- next-auth/app/dashboard/page.js
+++ next-auth/app/dashboard/page.js
@@ -1,5 +1,10 @@
-export default function Dashboard() {
-    return (
-        <h1>Welcome to your dashboard 🎉🎉🎉</h1>
-    )
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "../api/auth/[...nextauth]/options";
+
+export default async function Dashboard() {
+  const session = await getServerSession(nextAuthOptions);
+
+  console.log(session);
+
+  return <h1>Welcome to your dashboard 🎉🎉🎉</h1>;
 }
