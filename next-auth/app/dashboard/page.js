ae6e7d78535cbf4f4aed847a25881eeeefc9278c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-27 21:30:25 +0330

    add user information iand sing-out button in dashboard page

--- next-auth/app/dashboard/page.js
+++ next-auth/app/dashboard/page.js
@@ -1,10 +1,19 @@
 import { getServerSession } from "next-auth";
 import { nextAuthOptions } from "../api/auth/[...nextauth]/options";
+import Link from "next/link";
 
 export default async function Dashboard() {
   const session = await getServerSession(nextAuthOptions);
-
-  console.log(session);
-
-  return <h1>Welcome to your dashboard 🎉🎉🎉</h1>;
+  return (
+    <div>
+      <h3 className="text-lg">{`Welcome ${session.user.name} 👋`}</h3>
+      <h4 className="text-md">Your signed in ✅</h4>
+      <Link
+        href={"/api/auth/signout"}
+        className="p-1 bg-red-500 rounded-lg mt-3"
+      >
+        Sign out
+      </Link>
+    </div>
+  );
 }
