9a43e8db5d157d57e8153b3b6ede53e7c346c6f9
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-28 21:07:23 +0330

    refactor(proxy): rename proxy function to middleware and improve redirect formatting

--- next-auth/proxy.ts
+++ next-auth/proxy.ts
@@ -4,10 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const pathname = new URL(request.nextUrl).pathname;
 
-  if (pathname == "/") return NextResponse.redirect(new URL('/dashboard', request.url));
+  if (pathname == "/")
+    return NextResponse.redirect(new URL("/dashboard", request.url));
 }
 
 export const config = {
