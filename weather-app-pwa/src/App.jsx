65704db43b578a4272c21936209ef94ecd7e6ffa
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-11-30 21:36:15 +0330

    feat: add Persian language support and new assets for weather app

---
+++ weather-app-pwa/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function () {
+  return (
+    <div>
+      <input type="text" placeholder="نام شهر را وارد کنید ..." />
+    </div>
+  );
+}
---
+++ weather-app-pwa/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createReact, createRoot } from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import App from "./App";
+
+/**
+ * Root element
+ */
+const root = createRoot(document.getElementById("root"));
+
+/**
+ * Render application
+ */
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+/**
+ * Unregister service worker
+ */
+serviceWorkerRegistration.unregister();
