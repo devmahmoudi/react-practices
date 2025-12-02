2bb0e7302f656d748b65abd7f5f06aaad1d6d256
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-02 21:09:20 +0330

    feat(weather): implement weather fetching functionality with OpenWeather API integration

--- weather-app-pwa/src/App.jsx
+++ weather-app-pwa/src/App.jsx
@@ -1,9 +1,34 @@
+import axios from "axios";
 import "./App.css";
+import { useState } from "react";
 
 export default function () {
+  /**
+   * Weather state
+   */
+  const [weather, setWeather] = useState(null);
+
+  /**
+   * Get weather from openweathermap
+   * @param {string} city - The city name
+   * @returns {Promise<void>}
+   */
+  const getWeather = async (city) => {
+    const res = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`
+    );
+
+    setWeather(res.data);
+  };
   return (
-    <div>
-      <input type="text" placeholder="نام شهر را وارد کنید ..." />
+    <div className="container">
+      <div>
+        <input
+          type="text"
+          placeholder="نام شهر را وارد کنید ..."
+          onKeyDown={(e) => e.key === "Enter" && getWeather(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
