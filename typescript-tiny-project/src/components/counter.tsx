1586b26e1ba4d4e1ae4d2c786050df72c49ee5fd
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-11 20:33:34 +0330

    feat(components): add reset counter button

--- typescript-tiny-project/src/components/counter.tsx
+++ typescript-tiny-project/src/components/counter.tsx
@@ -9,11 +9,17 @@ const Counter = (props: CounterComponentProps) => {
     <div>
       <strong>{props.children}</strong>
       <div className="mt-3">
+        <button className="" onClick={() => props.setCount((prev) => prev > 1 ? prev - 1 : prev)}>
+          Decrement
+        </button>
+        <button
+          style={{ backgroundColor: "red" }}
+          onClick={() => props.setCount(() => 1)}
+        >
+          Reset
+        </button>
         <button className="" onClick={() => props.setCount((prev) => prev + 1)}>
           Increment
-        </button>
-        <button className="" onClick={() => props.setCount((prev) => prev - 1)}>
-          Decrement
         </button>
       </div>
     </div>
