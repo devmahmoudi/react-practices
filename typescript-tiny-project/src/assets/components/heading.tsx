e4ba968bacdba14985dff6e3753651ed2ece1561
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-11 19:46:28 +0330

    feat(components): add Heading component for title and subtitle display

---
+++ typescript-tiny-project/src/assets/components/heading.tsx
@@ -0,0 +1,25 @@
+import type { ReactElement } from "react";
+
+/**
+ * Heading Component Props
+ */
+type HeadingProps = {
+  title: string;
+  subTitle: string;
+};
+
+/**
+ * Heading Component
+ * @param props HeadingProps
+ * @returns ReactElement
+ */
+const Heading = (props: HeadingProps): ReactElement => {
+  return (
+    <div className="text-center">
+      <h1>{props.title}</h1>
+      <h5 style={{ color: "silver" }}>{props.subTitle}</h5>
+    </div>
+  );
+};
+
+export default Heading;
