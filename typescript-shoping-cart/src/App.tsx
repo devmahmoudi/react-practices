088bcea39c69775132002f0dfeb18e42a5ad3b5b
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-13 22:32:28 +0330

    feat: render products name in index page

--- typescript-shoping-cart/src/App.tsx
+++ typescript-shoping-cart/src/App.tsx
@@ -1,8 +1,20 @@
+import { useProducts } from "./contexts/products-context";
+
 function App() {
+  const { products } = useProducts();
+
   return (
     <div className="text-center">
       <h1>Typescript Practice Project</h1>
       <h3>Shoping Cart</h3>
+      <div>
+        {products.map((prod) => (
+          <div key={prod.id}>
+            <h2>{prod.name}</h2>
+            <small>{prod.price}</small>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
