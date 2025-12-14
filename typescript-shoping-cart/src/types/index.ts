abc6f96d940404956dfe2b358bcc8d15b0c9d829
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-14 22:35:24 +0330

    feat: add CartItemType  interface

--- typescript-shoping-cart/src/types/index.ts
+++ typescript-shoping-cart/src/types/index.ts
@@ -1,9 +1,16 @@
 /**
  * Product type
  */
-export type ProductType = {
+export interface ProductType {
   id: number;
   name: string;
   img: string;
   price: number | Float32Array;
 };
+
+/**
+ * Cart item type
+ */
+export interface CartItemType extends ProductType{
+  quantity?: number;
+}
