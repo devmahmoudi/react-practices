d6133c31421fce48b179b864224ef1d7ddf805a9
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-22 22:16:06 +0330

    refactor: change ApolloClient object uri to graphqlzero api

--- GraphQL-tiny/src/main.tsx
+++ GraphQL-tiny/src/main.tsx
@@ -8,7 +8,7 @@ import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
+  link: new HttpLink({ uri: "https://graphqlzero.almansi.me/api" }),
   cache: new InMemoryCache(),
 });
 
