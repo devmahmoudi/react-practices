f3f6951ce9b0d178058a659bed0d4d1c6756bac9
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-22 22:30:09 +0330

    feat(Users): send get all users query then render received users list data

--- GraphQL-tiny/src/components/users.tsx
+++ GraphQL-tiny/src/components/users.tsx
@@ -1,5 +1,12 @@
 import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 import React from "react";
+
+type User = {
+  name: string;
+  username: string;
+  id: number;
+};
 
 const GET_ALL_USERS_QUERY = gql`
   query {
@@ -14,7 +21,22 @@ const GET_ALL_USERS_QUERY = gql`
 `;
 
 const Users = () => {
-  return <div>Users</div>;
+  const { data, loading, error }: { data: { users: { data: User[] } } } =
+    useQuery(GET_ALL_USERS_QUERY);
+
+  if (loading) return <h1>LOADING ...</h1>;
+
+  return (
+    <div>
+      {data.users.data.map((user) => (
+        <ul>
+          <li>ID: {user.id}</li>
+          <li>Name: {user.name}</li>
+          <li>Username: {user.username}</li>
+        </ul>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
