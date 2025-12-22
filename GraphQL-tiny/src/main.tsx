5bf5b38b6a0130dbc8ea08e277596a701d7937fd
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-22 22:14:13 +0330

    feat(main.tsx): add ApolloProvider with passing a ApolloClient

--- GraphQL-tiny/src/main.tsx
+++ GraphQL-tiny/src/main.tsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Graph QL
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
